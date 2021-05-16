b0bd018ec7ec52f0f64ffc85acf6927ed8c8da6d
Chia-An Yang <chiaan.y.creativeworker@gmail.com>
2021-05-16 16:26:03 +0800

    :tada:  establish file structure

---
+++ models/Link.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const linkSchema = new Schema({
+  inputlink: String,
+  newlink: String
+});
+
+module.exports = mongoose.model('Link',linkSchema)
